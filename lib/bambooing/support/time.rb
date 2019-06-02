7b977d542fcc5573ab2c173fb14bec0aff54a80b
Jose Lloret <jose.lloret@flywire.com>
2019-06-03 00:50:47 +0200

    Bambooing::Support::Time.rand_work working for zero, one and two breaks

--- lib/bambooing/support/time.rb
+++ lib/bambooing/support/time.rb
@@ -4,18 +4,32 @@ module Bambooing
   module Support
     class Time
       class << self
-        def rand_work(hours:, start_hour:, breaks:)
-          today = ::Date.today
+        ONE_MINUTE_SECONDS = 60.freeze
+        ONE_HOUR_MINUTES = 60.freeze
+        ONE_HOUR_SECONDS = ONE_HOUR_MINUTES*ONE_MINUTE_SECONDS.freeze
+        MAX_BREAK_MINUTES = ONE_HOUR_MINUTES
+
+        def rand_work(date: nil, hours:, start_hour:, breaks:)
+          today = date || ::Date.today
           periods = []
 
-          if breaks == 1
-            period_seconds = hours/(breaks + 1) * 60
-          end
-          start = ::Time.new(today.year, today.mon, today.day, start_hour, Random.rand(60), 0)
-          _end = nil
-          periods << { start: "#{start.hour}:#{start.min}", end: _end }
+          seconds_per_period = hours/(breaks + 1.0) * ONE_HOUR_SECONDS
+          start = ::Time.new(today.year, today.mon, today.day, start_hour, Random.rand(MAX_BREAK_MINUTES), 0)
+          _end = ::Time.at(start.to_i + seconds_per_period)
+          periods << { start: start, end: _end }
 
-          periods
+          (0..breaks).reduce(periods) do |acc, i|
+            if i+1 <= breaks
+              period = acc[i]
+              start = ::Time.at(period[:end].to_i + Random.rand(MAX_BREAK_MINUTES)*ONE_MINUTE_SECONDS)
+              next_period = {
+                start: start,
+                end: ::Time.at(start.to_i + seconds_per_period)
+              }
+              acc[i+1] = next_period
+            end
+            acc
+          end
         end
       end
     end
--- spec/bambooing/support/time_spec.rb
+++ spec/bambooing/support/time_spec.rb
@@ -2,18 +2,33 @@ RSpec.describe Bambooing::Support::Time do
   describe '.rand_work' do
     let(:hours) { 8 }
     let(:start_hour) { 8 }
-    let(:breaks) { 2 }
 
     it 'returns an array of hashes' do
-      result = described_class.rand_work(hours: 8, start_hour: 8, breaks: 2)
+      result = described_class.rand_work(hours: 8, start_hour: 8, breaks: 1)
 
       expect(result).to all(include(:start).and include(:end))
     end
 
     it 'first start period starts at start_hour' do
-      result = described_class.rand_work(hours: hours, start_hour: start_hour, breaks: breaks)
+      result = described_class.rand_work(hours: hours, start_hour: start_hour, breaks: 1)
 
-      expect(result.first).to include(start: /^8:/)
+      start = result.first[:start].hour
+      expect(start).to eq(8)
+    end
+
+    context 'when no break is set' do
+      it 'returns one period' do
+        result = described_class.rand_work(hours: hours, start_hour: start_hour, breaks: 0)
+
+        expect(result.size).to eq(1)
+      end
+
+      it 'the amount of hours for the period is the hours passed' do
+        result = described_class.rand_work(hours: hours, start_hour: start_hour, breaks: 0)
+
+        elapsed_seconds = result[0][:end].to_i - result[0][:start].to_i
+        expect(hours*60*60).to eq(elapsed_seconds)
+      end
     end
 
     context 'when 1 break is set' do
@@ -21,6 +36,33 @@ RSpec.describe Bambooing::Support::Time do
         result = described_class.rand_work(hours: hours, start_hour: start_hour, breaks: 1)
 
         expect(result.size).to eq(2)
+      end
+
+      it 'the amount of hours for the two periods is hours passed' do
+        result = described_class.rand_work(hours: hours, start_hour: start_hour, breaks: 1)
+
+        first_elapsed_seconds = result[0][:end].to_i - result[0][:start].to_i
+        second_elapsed_seconds = result[1][:end].to_i - result[1][:start].to_i
+        total_elapsed_seconds = first_elapsed_seconds + second_elapsed_seconds
+        expect(hours*60*60).to eq(total_elapsed_seconds)
+      end
+    end
+
+    context 'when 2 breaks are set' do
+      it 'returns three periods' do
+        result = described_class.rand_work(hours: hours, start_hour: start_hour, breaks: 2)
+
+        expect(result.size).to eq(3)
+      end
+
+      it 'the amount of hours for three periods is hours passed' do
+        result = described_class.rand_work(hours: hours, start_hour: start_hour, breaks: 2)
+
+        first_elapsed_seconds = result[0][:end].to_i - result[0][:start].to_i
+        second_elapsed_seconds = result[1][:end].to_i - result[1][:start].to_i
+        third_elapsed_seconds = result[2][:end].to_i - result[2][:start].to_i
+        total_elapsed_seconds = first_elapsed_seconds + second_elapsed_seconds + third_elapsed_seconds
+        expect(hours*60*60).to eq(total_elapsed_seconds)
       end
     end
   end
