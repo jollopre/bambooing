7c54c05f898c2148b0ebb3417ab6d779e98e3f68
Juan Verdu <juan.verdu@flywire.com>
2023-06-11 20:05:12 +0200

    consider all pto types

--- lib/bambooing/time_off/table/pto.rb
+++ lib/bambooing/time_off/table/pto.rb
@@ -1,14 +1,27 @@
+require 'bambooing/time_off/employee'
+
 module Bambooing
   module TimeOff
     module Table
       class PTO < Request
-        TYPE = 77.freeze
-        APPROVED = 'approved'.freeze
+
+        PATH = '/time_off/employee'.freeze
+        APPROVED = 'approved'.freeze    
 
         class << self
+          
           def approved(employee_id:, year:)
-            ptos = where(employee_id: employee_id, type_id: TYPE, year: year)
-            ptos.select { |pto| pto.status == APPROVED }
+            pto_types = Employee.pto_types(employee_id)
+            ptos = []
+
+            pto_types.each { |pto_type|
+              
+              (ptos << where(employee_id: employee_id, type_id: pto_type, year: year)).flatten!
+            }
+
+            ptos.select { |pto| 
+              pto.status == APPROVED
+            }
           end
         end
       end
